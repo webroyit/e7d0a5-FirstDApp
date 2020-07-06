d4a190a6203d9e5478225279e51e955c91521bcc
royweb <xytank@gmail.com>
2020-07-06 14:59:08 -0400

    Connect to web3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import Web3 from 'web3';
 import './App.css';
 
-function App() {
-  return (
-    <div className="container">
-      <h1>Hello World</h1>
-    </div>
-  );
+class App extends React.Component{
+  componentWillMount(){
+    this.loadBlockchainData();
+  }
+
+  async loadBlockchainData(){
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+    const network = await web3.eth.net.getNetworkType();
+
+    console.log(network);
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
