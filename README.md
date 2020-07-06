9f799217ca99760b1b1c1dfbd2f9b28b00b1962f
royweb <xytank@gmail.com>
2020-07-06 15:43:08 -0400

    Display the address of the account from MetaMask

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # First DApp
+
+## Note
+- To get the address of your account from MetaMask, go to the setting -> connections and add the url of the localhost.
--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import Web3 from 'web3';
 import './App.css';
 
 class App extends React.Component{
-  componentWillMount(){
+  constructor(props){
+    super(props);
+    this.state = {
+      account: ''
+    }
+  }
+
+  componentDidMount(){
     this.loadBlockchainData();
   }
 
   async loadBlockchainData(){
     const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
     const network = await web3.eth.net.getNetworkType();
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({ account: accounts[0]})
 
     console.log(network);
   }
@@ -18,6 +28,7 @@ class App extends React.Component{
     return (
       <div className="container">
         <h1>Hello World</h1>
+        <p>Your account: {this.state.account}</p>
       </div>
     );
   }
